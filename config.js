d7e3910bf3592b0e83d1406d67249e96cf876f56
Julio Rodriguez <jircdev@gmail.com>
2025-12-04 19:35:16 -0300

    chore: update 0.1.5

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ System.register([], function (e, a) {
 		execute: function () {
 			e('default', {
 				package: '@aimpact/better-mind',
-				version: '0.1.4',
+				version: '0.1.5',
 				languages: { default: 'en', supported: ['en', 'es', 'de'] },
 				environment: 'development',
 				'global.css': true,
